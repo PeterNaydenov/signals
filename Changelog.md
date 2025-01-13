157dffe6b6e184c9856f3a5ef6306b3bd767efe3
Peter Naydenov <peter.naydenov@gmail.com>
2025-01-13 06:58:04 +0200

    JsDocs and  cleaning...

--- Changelog.md
+++ Changelog.md
@@ -2,5 +2,10 @@
 
 
 
+## 1.0.1 (2025-01-13)
+- [x] Refactoring: Multiple files;
+
+
+
 ## 1.0.0 (2025-01-12)
 - [x] Initial release
--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
-import computedLib from './computed.js'
 import effectLib   from './effects.js';
 import stateLib    from './states.js';
+import computedLib from './computed.js'
 
 
 
-/**
- * Creates the main API object for reactive items.
- * 
- * The API object contains three methods: `state`, `computed` and `effect`. 
- * 
- * `state` creates a reactive item with an initial value and optional validation.
- * `computed` creates a computed reactive item with a function that returns its value.
- * `effect` creates an effect reactive item with a function that is called immediately after any of its dependencies change.
- * 
- * The local storage is an object with two properties: `storage` and `callID`. 
- * `storage` is a map of reactive items.
- * `callID` is a unique identifier for the current call.
- */
+
 function main () {
     /**
      * A local storage for reactive items.
